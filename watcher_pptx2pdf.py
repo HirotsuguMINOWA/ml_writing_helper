a68a48a983dab07191489d7214f766e6d044031b
minowa.hirotsugu <minowa.hirotsugu@gmail.com>
2019-10-19 21:44:12 +0900

    one class_nized

--- watcher_pptx2pdf.py
+++ watcher_pptx2pdf.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# TODO: スクリプト開始時に、変換漏れを探せ
-import shutil
+# TODO: bin(soffice, imagemagick'convert and so on)の存在確認
 
-target_dir = "app_single/_fig_src"
-output_dir = "app_single/figs"
+
+# target_dir = "app_single/_fig_src"
+# output_dir = "app_single/figs"
 
 ###############################################################
 
@@ -22,10 +22,15 @@ import pathlib
 
 class ChangeHandler(FileSystemEventHandler):
 
+    def __init__(self, monitoring_dir, output_dir, dest_ext_no_period="png"):
+        self.target_dir = monitoring_dir
+        self.output_dir = output_dir
+        self.dest_ext_no_period = dest_ext_no_period
+
     def __ppt2pdf(self, src: pathlib, dest: pathlib):
         pass
 
-    def conv_ppt2pdf(self, path_src, dir_dest, dest_ext_no_period="pdf"):
+    def conv_ppt2pdf(self, path_src, dir_dest):  # , dest_ext_no_period="pdf"):
         """
         ppt->pdf->cropping
         :param dest_ext: without period!!
@@ -39,25 +44,25 @@ class ChangeHandler(FileSystemEventHandler):
             out_dir = plib_src.parent.as_posix()
             # path_tmp="tmp_"+os.path.basename(path_src)
             cmd = "/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to {dest_ext} --outdir {out_dir} {path_src}".format(
-                dest_ext=dest_ext_no_period, out_dir=out_dir, path_src=path_src)
+                dest_ext=self.dest_ext_no_period, out_dir=out_dir, path_src=path_src)
             print("[Debug] CMD: ppt2pdf" + cmd)
             tokens = shlex.split(cmd)
             subprocess.run(tokens)
 
             """ Add head "tmp_" to converted filename """
-            plib_pdf_convd = plib_src.with_suffix("." + dest_ext_no_period)
+            plib_pdf_convd = plib_src.with_suffix("." + self.dest_ext_no_period)
             plib_pdf_convd_tmp = pathlib.Path(plib_pdf_convd).with_name("tmp_" + plib_pdf_convd.name)
             # plib_pdf_convd.rename(plib_pdf_convd.with_name("tmp_"+plib_pdf_convd.name))
             plib_pdf_convd.rename(plib_pdf_convd_tmp)
             # plib_pdf_convd.with_name("tmp_"+plib_pdf_convd.name)
             """ Calc path of Dest """
             # path_dest = pathlib.Path(dir_dest) / plib_pdf_convd.name
-            path_dest = pathlib.Path(dir_dest) / plib_src.with_suffix("."+dest_ext_no_period).name
+            path_dest = pathlib.Path(dir_dest) / plib_src.with_suffix("." + self.dest_ext_no_period).name
 
             """ crop white space """
-            if dest_ext_no_period == "pdf":
+            if self.dest_ext_no_period == "pdf":
                 cmd = "pdfcrop {path_in} {path_out}".format(path_in=plib_pdf_convd_tmp, path_out=path_dest)
-            elif dest_ext_no_period in ["png", "jpeg", "jpg"]:
+            elif self.dest_ext_no_period in ["png", "jpeg", "jpg"]:
                 cmd = "convert {path_in} -trim {path_out} ".format(path_in=plib_pdf_convd_tmp, path_out=path_dest)
             else:
                 # new_path = shutil.move(plib_pdf_convd.as_posix(), dir_dest)
@@ -89,7 +94,7 @@ class ChangeHandler(FileSystemEventHandler):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         print('%sができました' % filename)
-        self.conv_ppt2pdf(path_src=event.src_path, dir_dest=output_dir, dest_ext_no_period="png")
+        self.conv_ppt2pdf(path_src=event.src_path, dir_dest=self.output_dir)  # , dest_ext_no_period="png")
 
     def on_modified(self, event):
         filepath = event.src_path
@@ -102,20 +107,21 @@ class ChangeHandler(FileSystemEventHandler):
         # TODO: 該当pdfを要削除
         print('%sを削除しました' % filename)
 
-
-def main():
-    while 1:
-        event_handler = ChangeHandler()
-        observer = Observer()
-        observer.schedule(event_handler, target_dir, recursive=True)
-        observer.start()
-        try:
-            while True:
-                time.sleep(0.1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
+    def main(self):
+        while 1:
+            #event_handler = ChangeHandler()
+            event_handler = self
+            observer = Observer()
+            observer.schedule(event_handler, self.target_dir, recursive=True)
+            observer.start()
+            try:
+                while True:
+                    time.sleep(0.1)
+            except KeyboardInterrupt:
+                observer.stop()
+            observer.join()
 
 
 if __name__ in '__main__':
-    main()
+    ins = ChangeHandler(monitoring_dir="app_single/_fig_src",output_dir = "app_single/figs")
+    ins.main()
