46a17c9cc70a8506af5766e625917e5324c7b7aa
Hirotsugu Minowa <minowa.hirotsugu@gmail.com>
2020-01-06 01:13:26 +0900

    remove todo

--- ml_writing_helper/__init__.py
+++ ml_writing_helper/__init__.py
@@ -2,7 +2,6 @@
 
 # TODO: bin(soffice, imagemagick'convert and so on)の存在確認
 # TODO: pdfは透過png?が黒くなるためデフォルトはpng
-# TODO: __init__.pyへ移せ2020,4月前に
 # FIXME: croppingした結果をsrc pathに保存しているのではなく, dst pathに保存しろ
 # TODO: dstとsrcフォルダの生成、実行時に。
 # target_dir = "app_single/_fig_src"
