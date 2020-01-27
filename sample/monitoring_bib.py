7f5652d5f769bed6e981f7f84a7d2053adb0464b
minowa <minowa.hirotsugu@gmail.com>
2020-01-27 15:35:03 +0900

    revised

--- sample/monitoring_bib.py
+++ sample/monitoring_bib.py
@@ -1,7 +1,9 @@
-from src.core import ChangeHandler
+from core import ChangeHandler
 
-ChangeHandler().monitor(
+p = ChangeHandler()
+p.set_monitor(
     src_dir="/Users/hirots-m/Documents/BibTexExported"
     , dst_dir="fig_gen"
     , to_fmt=".bib"
 )
+p.start_monitors()
--- sample/monitoring_very_simple.py
+++ sample/monitoring_very_simple.py
@@ -1,6 +1,12 @@
-from src.core import ChangeHandler
-ChangeHandler().monitor(
+# Caution
+# Set src dir as source folder in projectPref.
+# You can import by "from core import ChangeHandler"
+from core import ChangeHandler
+
+p = ChangeHandler()
+p.set_monitor(
     src_dir="fig_src"
     , dst_dir="fig_gen"
     , to_fmt="eps"
 )
+p.start_monitors()
