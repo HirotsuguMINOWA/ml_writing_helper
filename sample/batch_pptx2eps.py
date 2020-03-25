085e4a06059a147244c3f4c351839a92f67f89b6
hirotsuguminowa <minowa.hirotsugu@gmail.com>
2020-03-25 22:23:58 +0900

    fixed but not run

--- sample/batch_pptx2eps.py
+++ sample/batch_pptx2eps.py
@@ -1,21 +1,20 @@
 import shutil
 
-from ml_writing_helper.__init__ import ChangeHandler
+from core import ChangeHandler
 from pathlib import Path
 
 p_src = Path(__file__).resolve().parent.joinpath("fig_src")
-p_dst = Path(__file__).resolve().parent.joinpath("fig_tmp")
+p_dst = Path(__file__).resolve().parent.joinpath("fig_gen")
 
+print("SRC:%s" % p_src)
 if p_dst.exists():
     shutil.rmtree(p_dst)
     p_dst.mkdir()
 
-conv = ChangeHandler(
-    monitoring_dir=p_src.as_posix(), output_dir=p_dst.as_posix()
-    , dst_ext_no_period="png"
-)
+conv = ChangeHandler()
+
 conv.convert(
-    src_file_apath=p_src.joinpath("test.pptx"),
+    src_file_apath=p_src.joinpath("test_ppt.pptx"),
     dst_dir_apath=p_dst.as_posix(),
     to_fmt="eps"
 )
