7be3446de5f0ff8c115b4838ebe0291243702227
Hirotsugu MINOWA <minowa.hirotsugu@gmail.com>
2022-07-30 11:45:30 +0900

    Changed to relative path.

--- try/magic_try1.py
+++ try/magic_try1.py
@@ -2,7 +2,7 @@ import re
 
 import magic
 
-t = magic.from_file("/Users/hirots-m/Documents/PyCharmProjects/ml_writing_helper/try/res_comp_force.png")
+t = magic.from_file("src/try/res_comp_force.png")
 print(t)
 img_size = re.search('(\d+) x (\d+)', t).groups()
 print(img_size)
