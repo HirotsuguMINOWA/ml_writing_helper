23e938870a7754538d4bc4df0866135bcc7aa926
hirots-m <minowa.hirotsugu@gmail.com>
2023-11-18 15:35:01 +0900

    Fixed. The project name ml_writing_helper info MLWritingHelper.

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as requirements_file:
-    install_requirements = requirements_file.read().splitlines()
+# with open('requirements.txt') as requirements_file:
+    # install_requirements = requirements_file.read().splitlines()
+
+def requirements_from_file(file_name):
+    return open(file_name).read().splitlines()
 
 setup(
-    name="ml_writing_helper",
+    name="MLWritingHelper",
     version="0.0.1",
     description="Helper library to write markup language of LaTeX and Markdown",
     author="Hirotsugu Minowa",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=install_requirements,
+    # install_requires=install_requirements,
+    install_requires=requirements_from_file('requirements.txt'),
     # 要検討
     # もしCLIツールを作る場合は、下記通りentrypointを指定するとよい。
     # 実行した下位Folderすべて監視して、pptxをpngに変換してはどうか
