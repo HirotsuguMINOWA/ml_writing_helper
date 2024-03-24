328c0a6a800bf65dd12ea63986737b097501ef3d
HirotsuguMinowa <minowa.hirotsugu@gmail.com>
2024-03-24 18:56:48 +0900

    Added logger

--- sample/start_watcher.py
+++ sample/start_watcher.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import click
-
+from loguru import logger
 from ml_writing_helper import Monitor
 from ml_writing_helper.output_ext import OutputExtTuple
 
@@ -13,12 +13,16 @@ o = Monitor()
 def run(ext: str = "pdf"):
     """defines manuscript root path"""
     manuscript_root = Path(__file__).resolve().parent
+    logger.debug(f"manuscript_root: {manuscript_root}")
 
     """monitoring fig_src folder and it converts their files into fid_gen."""
     o.set_monitor(
         src_dir=manuscript_root.joinpath("fig_src"),
         dst_dir=manuscript_root.joinpath("fig_gen"),
         to_fmt=f".{ext}",
+        is_crop=True,
+        mk_dst_dir=True,
+        gray=True,
     )
 
     """out of project bib file monitoring. If changed, it copies to the bib folder."""
