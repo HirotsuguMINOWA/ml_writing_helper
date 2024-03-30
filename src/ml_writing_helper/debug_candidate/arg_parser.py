65621acc833e35284b8946319a466157fbad2498
Hirotsugu MINOWA <minowa.hirotsugu@gmail.com>
2024-03-30 20:30:31 +0900

    updated

--- src/ml_writing_helper/debug_candidate/arg_parser.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-
-def arg_parser():
-    parser = argparse.ArgumentParser(description='ML Writing Helper')
-    # parser.add_argument('--debug', action='store_true', help='Debug mode')
-    # parser.add_argument('--input', type=str, help='Input file')
-    parser.add_argument('--output', choices=['eps', 'pdf', 'png'], help='Output Extension')
-    return parser.parse_args()
